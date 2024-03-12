c29a31faecdb395d6a83d94f3630962190fbcd29
Carlo Martin <carlocarlo.mtz.martin@gmail.com>
2024-03-11 17:33:41 -0700

    changedb

--- src/plugins/mysql.js
+++ src/plugins/mysql.js
@@ -1,8 +1,9 @@
 /* eslint-disable no-unused-vars */
 const fp = require('fastify-plugin')
 
-const localDb = 'mysql://root:123456@core-database:3306/dreamdb'
-const serverDb = 'mysql://dream:123456@localhost:3306/dreamdb'
+// const localDb = 'mysql://root:123456@core-database:3306/dreamdb'
+// const serverDb = 'mysql://dream:123456@localhost:3306/dreamdb'
+const serverDb = 'mysql://freedb_dream:%dt@Hm98!Wt!yNG@sql.freedb.tech:3306/freedb_just_a_test'
 
 const mysqlPlugin = async (fastify, options) => {
   fastify.register(require('@fastify/mysql'), {
